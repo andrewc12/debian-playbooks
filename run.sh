a39b9fcbb0fea281f6e9bc509aa966c8a47ff019
Andrew Innes <andrew.c12@gmail.com>
2016-05-20 16:43:53 +0800

    script to run ansible

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #ansible-playbook -i 'localhost,' --connection=local playbook.yml --check
-ansible-playbook -i 'localhost,' --connection=local playbook.yml
+#ansible-playbook -i 'localhost,' --connection=local playbook.yml
+cp /vagrant/ansible /tmp/ansible
+chmod 700 /tmp/ansible
+ansible-playbook -i /vagrant/inventory /vagrant/debian-playbooks/playbook.yml
